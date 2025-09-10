4d4d084159a39aa23f411e7831e7a25ee3bfe2f1
Mingzhe Zou <zoumingzhe@qq.com>
2025-09-10 20:57:52 +0800

    tmp1

--- dnsearch/record.py
+++ dnsearch/record.py
@@ -1,19 +1,23 @@
 # coding:utf-8
 
+from typing import Optional
+
 from dnslib import A
 from dnslib import AAAA
 from dnslib import CNAME
 from dnslib import DNSRecord
 from dnslib import QTYPE
 from dnslib import RR
+from xkits_lib import LiveMeter
 
 from dnsearch.domain import Name
 
 
-class Record():
-    def __init__(self, name: Name, data: str):
+class Record(LiveMeter):
+    def __init__(self, name: Name, data: str, ttl: int):
         self.__name: str = str(name)
         self.__data: str = data
+        super().__init__(ttl)
 
     @property
     def name(self) -> str:
@@ -23,16 +27,21 @@ class Record():
     def data(self) -> str:
         return self.__data
 
+    def update(self, data: str, ttl: int) -> None:
+        self.__data = data
+        self.ttl = ttl
+
 
 class RecordA(Record):
     RTYPE: int = QTYPE.A
 
-    def __init__(self, name: Name, ipv4: str):
-        super().__init__(name=name, data=ipv4)
+    def __init__(self, name: Name, ipv4: str, ttl: int):
+        super().__init__(name=name, data=ipv4, ttl=ttl)
 
-    def to_rr(self, ttl: int = 0) -> RR:
+    def to_rr(self, ttl: Optional[int] = None) -> RR:
         """DNS Resource Record"""
-        return RR(self.name, self.RTYPE, rdata=A(self.data), ttl=ttl)
+        return RR(rname=self.name, rtype=self.RTYPE, rdata=A(self.data),
+                  ttl=int(self.ttl) if ttl is None else ttl)
 
 
 class Resources():
